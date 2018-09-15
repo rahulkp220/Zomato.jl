58d01f007c371b4d49f521074f398dd7d827820c
rahul lakhanpal <rahulkp220@gmail.com>
2018-09-16 02:22:35 +0530

    adding docs-part1

--- src/Zomato.jl
+++ src/Zomato.jl
@@ -27,8 +27,21 @@ Base.show(io::IO, z::Z) = print(io, "Zomato(", z.base_url, ')')
 
 """
 Get list of categories
+---------------------
+List of all restaurants categorized under a particular restaurant type can be obtained using /Search API with Category ID as inputs
 
 See https://developers.zomato.com/documentation#!/common/categories
+
+Model Schema
+------------
+``` 
+[
+  {
+    "category_id": "3",
+    "category_name": "Nightlife"
+  }
+]
+```
 """
 function categories(z::Z)
 	helper(z, "categories", Dict())
@@ -37,8 +50,42 @@ end
 
 """
 Get city details
+----------------
+Find the Zomato ID and other details for a city . You can obtain the Zomato City ID in one of the following ways:
+
+- City Name in the Search Query - Returns list of cities matching the query
+- Using coordinates - Identifies the city details based on the coordinates of any location inside a city
+
+If you already know the Zomato City ID, this API can be used to get other details of the city.
 
 See https://developers.zomato.com/documentation#!/common/cities
+
+Model Schema
+------------
+```
+[
+  {
+    "id": "280",
+    "name": "New York City, NY",
+    "country_id": "216",
+    "country_name": "United States",
+    "is_state": "0",
+    "state_id": "103",
+    "state_name": "New York State",
+    "state_code": "NY"
+  }
+]
+```
+Arguments
+---------
+
+| Parameter | Description                      | Parameter Type | Data Type |
+|-----------|----------------------------------|----------------|-----------|
+| q         | query by city name               | query          | String    |
+| lat       | latitude                         | query          | Float     |
+| lon       | longitude                        | query          | Float     |
+| city_ids  | comma separated city_id values   | query          | String    |
+| count     | number of max results to display | query          | Int       |
 """
 function cities(z::Z; kwargs...)
 	helper(z, "cities", Dict(kwargs))
@@ -47,8 +94,41 @@ end
 
 """
 Get zomato collections in a city
+--------------------------------
+Returns Zomato Restaurant Collections in a City. The location/City input can be provided in the following ways -
+
+- Using Zomato City ID
+- Using coordinates of any location within a city
+
+List of all restaurants listed in any particular Zomato Collection can be obtained using the '/search' API with Collection ID and Zomato City ID as the input
 
 See https://developers.zomato.com/documentation#!/common/collections
+
+Model Schema
+-----------
+```
+[
+  {
+    "collection_id": "1",
+    "title": "Trending this week",
+    "url": "https://www.zomato.com/new-york-city/top-restaurants",
+    "description": "The most popular restaurants in town this week",
+    "image_url": "https://b.zmtcdn.com/data/collections/e40960514831cb9b74c552d69eceee0f_1418387628_l.jpg",
+    "res_count": "30",
+    "share_url": "http://www.zoma.to/c-280/1"
+  }
+]
+```
+
+Arguments
+---------
+
+| Parameter | Description                                     | Parameter Type | Data Type |
+|-----------|-------------------------------------------------|----------------|-----------|
+| city_id   | id of the city for which collections are needed | query          | Int       |
+| lat       | latitude                                        | query          | Float     |
+| lon       | longitude                                       | query          | Float     |
+| count     | number of max results to display                | query          | Int       |
 """
 function collections(z::Z; kwargs...)
 	helper(z, "collections", Dict(kwargs))
